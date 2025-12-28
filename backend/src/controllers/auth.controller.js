bd6425cfc7b71845ed470e40068b9cdab26a2c64
Mohammad Ahmad <m.ahmad@technophiles.ae>
2025-12-28 20:31:43 +0300

    Welcome Email + refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import "dotenv/config";
+import { ENV } from "./env.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -45,6 +48,16 @@ export const signup = async (req, res) => {
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
+
+      try {
+        await sendWelcomeEmail(
+          savedUser.email,
+          savedUser.fullName,
+          ENV.CLIENT_URL
+        );
+      } catch (error) {
+        console.error("Failed to send welcome Email");
+      }
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
